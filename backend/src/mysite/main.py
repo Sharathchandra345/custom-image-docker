87a2597888b6fd9549802471e94c63d897aa9b6f
Sharath Chandra Chinnarigari <csharathchandra3@gmail.com>
2025-03-15 03:07:16 +0530

    added docker compose

--- backend/src/mysite/main.py
+++ backend/src/mysite/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
+
+MONGODB_CONNECTION_STRING=os.environ["MONGODB_CONNECTION_STRING"]
+
+client=AsyncIOMotorClient(MONGODB_CONNECTION_STRING,uuidRepresentation="standard")
+db=client.todolist
+todos=db.todos
 
 app = FastAPI()
 
@@ -14,7 +20,7 @@ app.add_middleware(
 )
 
 class TodoItem(BaseModel):
-    id: int
+    id: UUID = Field(default_factory=uuid4,alias="_id")
     content: str
 
 class TodoItemCreate(BaseModel):
@@ -27,18 +33,16 @@ id_counter = 1
 async def create_todo(item: TodoItemCreate):
     global id_counter
     new_todo = TodoItem(id=id_counter, content=item.content)
-    todos.append(new_todo)
-    id_counter += 1
+    await todos.insert_one(new_todo.model_dump(by_alias=True))
     return new_todo
 
 @app.get("/todos", response_model=List[TodoItem])
 async def read_todos():
-    return todos
+    return await todos.find().to_list(length=None)
 
 @app.delete("/todos/{todo_id}")
 async def delete_todo(todo_id: int):
-    for index, todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos.pop(index)
-            return {"message": "Todo deleted successfully"}
-    raise HTTPException(status_code=404, detail="Todo not found")
+    delete_result=await todso.delete_one({"_id":todo_id})
+    if delete_result.deleted_count==0:
+      raise HTTPException(status_code=404, detail="Todo not found")        
+    return {"message": "Todo deleted successfully"}
